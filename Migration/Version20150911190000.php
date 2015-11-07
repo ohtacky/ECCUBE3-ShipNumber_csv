b32dbfd4822e5a328c6b95b4b355289ebb355f5a
takashi <takashi@handmadecompany.jp>
2015-11-07 10:55:41 +0900

    postgres時のエラー修正

--- Migration/Version20150911190000.php
+++ Migration/Version20150911190000.php
@@ -25,43 +25,51 @@ class Version20150911190000 extends AbstractMigration
     public function down(Schema $schema)
     {
         $schema->dropTable('plg_ship_number_csv');
-// 送信履歴で使用するため、削除できない
-//        $this->addSql('DELETE FROM dtb_mail_template WHERE name = "発送メール" ');
+
+        // 送信履歴で使用するため、削除できない
+        //$this->connection->delete('dtb_mail_template', array('name' => '発送メール'));
     }
 
     public function postUp(Schema $schema)
     {
 
-        $app = new \Eccube\Application();
-        $app->boot();
-        $creatorId = '1';
-        $name = '発送メール';
-        $fileName = 'Mail/order.twig';
-        $subject = '商品を発送致しました。';
-        $header = 'この度はご注文いただき誠にありがとうございます。
+      $app = new \Eccube\Application();
+      $app->boot();
 
-                    ご注文いただいた下記商品を本日発送いたしました。
-                    伝票番号：
-                    発送業者：ヤマト運輸
-                    詳しい発送状況は、荷物お問い合わせページ（ヤマト運輸） からご確認ください。
-                    http://toi.kuronekoyamato.co.jp/cgi-bin/tneko';
-        $footer = '============================================
+      $statement = $this->connection->prepare('SELECT template_id FROM dtb_mail_template');
+      $statement->execute();
+      $templateId = $statement->fetchAll();
+
+      $templateIdNumber = count($templateId) + 1;
+
+      $creatorId = '1';
+      $name = '発送メール';
+      $fileName = 'Mail/order.twig';
+      $subject = '商品を発送致しました。';
+      $header = 'この度はご注文いただき誠にありがとうございます。
+
+ご注文いただいた下記商品を本日発送いたしました。
+伝票番号：
+発送業者：ヤマト運輸
+詳しい発送状況は、荷物お問い合わせページ（ヤマト運輸） からご確認ください。
+http://toi.kuronekoyamato.co.jp/cgi-bin/tneko';
+      $footer = '============================================
 
 
-                    このメッセージはお客様へのお知らせ専用ですので、
-                    このメッセージへの返信としてご質問をお送りいただいても回答できません。
-                    ご了承ください。
+このメッセージはお客様へのお知らせ専用ですので、
+このメッセージへの返信としてご質問をお送りいただいても回答できません。
+ご了承ください。
 
-                    ご質問やご不明な点がございましたら、こちらからお願いいたします。
+ご質問やご不明な点がございましたら、こちらからお願いいたします。
 
-                    ';
-        $delFlg = '0';
-        $datetime = date('Y-m-d H:i:s');
-        $insert = "INSERT INTO dtb_mail_template(
-                            creator_id, name, file_name, subject, header, footer, del_flg, create_date, update_date)
-                    VALUES ('$creatorId', '$name', '$fileName', '$subject', '$header', '$footer', '$delFlg', '$datetime', '$datetime'
-                            );";
-        $this->connection->executeUpdate($insert);
+';
+      $delFlg = '0';
+      $datetime = date('Y-m-d H:i:s');
+      $insert = "INSERT INTO dtb_mail_template(
+                          template_id, creator_id, name, file_name, subject, header, footer, del_flg, create_date, update_date)
+                  VALUES ('$templateIdNumber', '$creatorId', '$name', '$fileName', '$subject', '$header', '$footer', '$delFlg', '$datetime', '$datetime'
+                          );";
+      $this->connection->executeUpdate($insert);
 
     }
 
